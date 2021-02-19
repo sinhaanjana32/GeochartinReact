1f51cd3737ce5a6d60664533045f2f17671c9e30
Anjana <sinhaanjana32@gmail.com>
2021-02-19 15:27:40 +0200

    add updates

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+// import Video from "./Video";
+import GeoChart from "./GeoChart";
+import data from "./GeoChart.world.geo.json";
+import "./App.css";
+import { Typography } from "@material-ui/core";
+
+function App() {
+  const [property, setProperty] = useState("pop_est");
+  return (
+    <React.Fragment>
+      <Typography variant="h4" color="secondary">
+        World Map with d3-geo
+      </Typography>
+      <GeoChart data={data} property={property} />
+      <Typography variant="h3" color="textSecondary">
+        Select property to highlight
+      </Typography>
+      <select
+        value={property}
+        onChange={(event) => setProperty(event.target.value)}
+      >
+        <option value="pop_est">Population</option>
+        <option value="name_len">Name length</option>
+        <option value="gdp_md_est">GDP</option>
+      </select>
+    </React.Fragment>
+  );
+}
+
+export default App;
